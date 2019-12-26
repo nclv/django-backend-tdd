deaf32d250b07ca0023e9005fd1a75cdb0c2e023
Nicolas Vincent <nicolas.vincent100@gmail.com>
2019-12-26 21:28:00 +0100

    Comments on serializer

--- taxi/trips/serializers.py
+++ taxi/trips/serializers.py
@@ -7,11 +7,13 @@ class UserSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True)
 
     def validate(self, data):
+        """On override validate pour vérifier que le mdp est valide."""
         if data['password1'] != data['password2']:
             raise serializers.ValidationError('Passwords must match.')
         return data
 
     def create(self, validated_data):
+        """On override create pour garder un seul mdp."""
         data = {
             key: value for key, value in validated_data.items()
             if key not in ('password1', 'password2')
